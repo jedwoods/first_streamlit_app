69cc8b7297e6b448adac9cd69599d453bce350fa
Jed Woods <134558375+jedwoods@users.noreply.github.com>
2023-07-24 09:53:58 -0600

    Update streamlit_app.py
    removed parentheses that were blocking variable input

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # makes the json data more intutive to the reader 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
