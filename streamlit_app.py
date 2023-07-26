3ff2cf567754067a5774cf1d1095e2958a863cbb
Jed Woods <134558375+jedwoods@users.noreply.github.com>
2023-07-26 15:44:23 -0600

    Update streamlit_app.py
    added a try block and yearh

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,15 +22,22 @@ fruits_to_show =  my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# makes the json data more intutive to the reader 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# displays the data in a table
-streamlit.dataframe(fruityvice_normalized)
+try:
+  
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information.")
+  else:
+    streamlit.write('The user entered', fruit_choice)
+    
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    
+    # makes the json data more intutive to the reader 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # displays the data in a table
+    streamlit.dataframe(fruityvice_normalized)
+else ERLErorror as e:
+  streamlit.error()
 
 
 
