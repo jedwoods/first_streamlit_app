8b2574dc55e815a3edec2530d6867c2b3e834f4b
Jed Woods <134558375+jedwoods@users.noreply.github.com>
2023-07-26 16:05:21 -0600

    Update streamlit_app.py
    restructured to use a function in the  try block

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,22 +20,25 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show =  my_fruit_list.loc[fruits_selected]
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruity_vice_normalized
+
 streamlit.dataframe(fruits_to_show)
+
+
 streamlit.header('Fruityvice Fruit Advice!')
+
 try:
   
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("please select a fruit to get information.")
   else:
-    
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    
-    # makes the json data more intutive to the reader 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # displays the data in a table
-    streamlit.dataframe(fruityvice_normalized)
-except ERLErorror as e:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+except URLErorror as e:
   streamlit.error()
 
 
